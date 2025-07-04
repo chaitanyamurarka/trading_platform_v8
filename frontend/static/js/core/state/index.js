2a4918dc8362028980c49cdeb82d85787bb7686a
Chaitanya Murarka <103809867+chaitanyamurarka@users.noreply.github.com>
2025-07-04 11:52:32 +0000

    Revert "Trying Further Migration"
    This reverts commit 90d3ca29c3836df7b0bc2765f62ba99fedcf5243.

--- frontend/static/js/core/state/index.js
+++
@@ -1,7 +0,0 @@
-/**
- * @file core/state/index.js
- * @description State management exports
- */
-
-export { appState, stateHelpers } from './app.state.js';
-export { CONSTANTS } from './constants.js';
--- frontend/static/js/utils/date.utils.js
+++
@@ -1,8 +0,0 @@
-/**
- * @file utils/date.utils.js
- * @description Date and time manipulation utilities
- */
-
-/**
- * Format date in specific timezone
- * @param {Date} date - Date to format
